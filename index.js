49581559daf75481bdf096989b05800fd3692e9e
Ubuntu <ubuntu@ip-172-31-25-194.ap-northeast-2.compute.internal>
2019-07-17 06:03:11 +0000

    final

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello tjkkkkddd!');
+  res.send('!Hello tjk1238172931729d!');
 });
 
 var server = app.listen(3000, function () {
